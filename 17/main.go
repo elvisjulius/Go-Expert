ac1555a2ac41d7123b7bbde9c893cd6a8041300d
Elvis <elvisjulius@hotmail.com>
2023-02-22 22:37:04 -0300

    adicionando comparable com generics

--- 17/main.go
+++ 17/main.go
@@ -1,7 +1,9 @@
 package main
 
+type MyNumber int
+
 type Number interface {
-	int | float64
+	~int | ~float64
 }
 
 func Soma[T Number](m map[string]T) T {
@@ -12,10 +14,20 @@ func Soma[T Number](m map[string]T) T {
 	return soma
 }
 
+func Compara[T comparable](a T, b T) bool {
+	if a == b {
+		return true
+	}
+	return false
+}
+
 func main() {
 
 	m := map[string]int{"Elvis": 3000, "João": 2000, "Maria": 5000}
 	m2 := map[string]float64{"Elvis": 3000.34, "João": 2000.54, "Maria": 5000.67}
+	m3 := map[string]MyNumber{"Elvis": 3000, "João": 2000, "Maria": 5000}
 	println(Soma(m))
 	println(Soma(m2))
+	println(Soma(m3))
+	println(Compara(10, 100))
 }
