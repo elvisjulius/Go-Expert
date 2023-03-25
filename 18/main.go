56a31a9e0ed3b0a3aed3fafc26e48c493cf5abc1
Elvis <elvisjulius@hotmail.com>
2023-03-25 19:45:33 -0300

    Pacotes e modulos2

--- 18/main.go
+++ 18/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"fmt"
-
 	"curso-go/matematica"
+	"fmt"
 )
 
 func main() {
 	s := matematica.Soma(10, 20)
+	carro := matematica.Carro{Marca: "Fiat"}
 
+	fmt.Println(carro.Andar())
 	fmt.Println("O resultado: ", s)
+	fmt.Println(matematica.A)
 }
--- 18/matematica/matematica.go
+++ 18/matematica/matematica.go
@@ -3,3 +3,15 @@ package matematica
 func Soma[T int | float64](a, b T) T {
 	return a + b
 }
+
+var A int = 10
+
+type Carro struct {
+	Marca string
+}
+
+func (c Carro) Andar() string {
+	return "Carro andando"
+}
+
+// letras maiusculas permitem importar uma struct,função ou atributo fora do pacote
